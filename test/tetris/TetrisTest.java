44095a2bb11b34465bdbca27f588979140b6505e
AlexSfc04 <amoricoalejandro@alumnos.cei.es>
2025-03-14 11:23:38 +0100

    test

---
+++ test/tetris/TetrisTest.java
@@ -0,0 +1,36 @@
+package tetris;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.awt.*;
+
+class TetrisTest {
+	
+	private GameController game;
+	private Board board;
+	
+	@BeforeEach
+	void setUp() {
+		game = new GameController();
+		board = new Board();
+	}
+	
+	@Test
+	void testInitialBoard() {
+		assertEquals(0, game.currentScore(), "The score doesn't initialize as 0.");
+	}
+	
+	@Test
+	void testPieceCanMoveDown() {
+		Tetromino piece = new Tetromino(new Point[] {new Point(4, 0), new Point(5, 0)}, Color.RED);
+		assertTrue(board.canMove(piece, 0 , 1), "La pieza debería poder moverse hacia abajo.");
+	}
+	
+	@Test
+	void testPieceCannotMoveOutsideLeft() {
+		Tetromino piece = new Tetromino(new Point[] {new Point(0, 0), new Point(1, 0)}, Color.BLUE);
+        assertFalse(board.canMove(piece, -1 , 0), "La pieza no debería moverse fuera del borde izquierdo.");
+    }
+		
+}
